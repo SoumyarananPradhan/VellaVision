e0303f8bc9fffed7fb558a984544f012e53ee43c
Soumyaranjan <soumyaranjanaman54@gmail.com>
2026-01-31 12:42:56 +0530

    Turn off Debug mode for production

--- youtube/youtube/settings.py
+++ youtube/youtube/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-njp)gd5m(2-w-i*a3u22ni&j8o$nezp!d2noxf)w4&%-f-0f$b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.railway.app']
 
